3ca40aac278ac506ccd1f25d11dc304a274d91d5
Pujianto1219 <yasril.pujianto38@smk.belajar.id>
2026-01-14 12:21:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -119,7 +119,7 @@ domain=$(cat /etc/xray/domain)
 ip=$(wget -qO- ipinfo.io/ip)
 
 # Mendapatkan data izin dan key
-data=$(curl -s https://raw.githubusercontent.com/Pujianto1219/scAUTOku/refs/heads/main/izin)
+data=$(curl -s https://raw.githubusercontent.com/Pujianto1219/ip/main/ip)
 key=$(echo "$data" | grep "$ip" | awk '{print $2}')
 
 # URL sumber konfigurasi dan binary
